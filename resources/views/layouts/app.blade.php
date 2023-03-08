08f84741de6bc1902d646c08e1250381e8dc221b
Antonio-Italiano <antoitalia221@gmail.com>
2023-03-08 14:05:08 +0100

    Added site icon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,9 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
+    {{-- ICON --}}
+    <link rel="icon" href="{{ asset('images/logo.png') }}" type="images/png">
+
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
